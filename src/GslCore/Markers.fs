4a05f8539eee52a1581af40c99db3b92912c9e45
Marcell Legeza <legezam@gmail.com>
2021-02-15 16:15:11 +0100

    REF: refactored Markers

--- src/GslCore/Markers.fs
+++ src/GslCore/Markers.fs
@@ -12,36 +12,37 @@ hphAX   13992   2of2    2       9
 *)
 
 type HalfMarker =
-    { name: string
-      rid: int
-      linkerFr: string
-      linkerTo: string
-      part1or2: int }
+    { Name: string
+      Rid: int
+      LinkerFrom: string
+      LinkerTo: string
+      Part1or2: int }
 
-let loadMarkers (libPath: string) =
-    let markerPath = opj libPath "markers.txt"
+module HalfMarker =
+    let loadMarkers (libPath: string): Set<HalfMarker> =
+        let markerPath = opj libPath "markers.txt"
 
-    if not (File.Exists(markerPath)) then
-        Set.empty
-    else
-        (eachLineIn markerPath)
-        |> Seq.map (tabSplit)
-        |> Seq.map (function
-            | [| name; id; "1of2"; frId; toId |] ->
-                { name = name
-                  rid = int id
-                  linkerFr = frId
-                  linkerTo = toId
-                  part1or2 = 1 }
-            | [| name; id; "2of2"; frId; toId |] ->
-                { name = name
-                  rid = int id
-                  linkerFr = frId
-                  linkerTo = toId
-                  part1or2 = 2 }
-            | x -> failwithf "ERROR: unmatched line in %s:  %A\n" markerPath x)
-        |> Set.ofSeq
+        if not (File.Exists(markerPath)) then
+            Set.empty
+        else
+            (eachLineIn markerPath)
+            |> Seq.map (tabSplit)
+            |> Seq.map (function
+                | [| name; id; "1of2"; frId; toId |] ->
+                    { Name = name
+                      Rid = int id
+                      LinkerFrom = frId
+                      LinkerTo = toId
+                      Part1or2 = 1 }
+                | [| name; id; "2of2"; frId; toId |] ->
+                    { Name = name
+                      Rid = int id
+                      LinkerFrom = frId
+                      LinkerTo = toId
+                      Part1or2 = 2 }
+                | x -> failwithf "ERROR: unmatched line in %s:  %A\n" markerPath x)
+            |> Set.ofSeq
 
-let legalMarkers path =
-    loadMarkers path
-    |> Set.map (fun halfMarker -> halfMarker.name)
+    let legalMarkers (path: string): Set<string> =
+        loadMarkers path
+        |> Set.map (fun halfMarker -> halfMarker.Name)
